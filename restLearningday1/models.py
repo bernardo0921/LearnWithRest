af7efd1f9c62447bf069932dde8368391a4a769a
Bernard <ephraimbernard7@gmail.com>
2024-08-22 02:50:09 +0000

    made new migration

--- restLearningday1/models.py
+++ restLearningday1/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class Drink(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
     
